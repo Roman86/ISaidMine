38f5e931e8f592a0a84b565ca5356ea9c7f64ea2
Roman86 <romych.sync@gmail.com>
2014-06-04 21:57:48 +0600

    hardcoded version

---
+++ ISaidMineCore/ISaidMineGUI/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace ISaidMineGUI
+{
+	partial class fMain
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.btnExit = new System.Windows.Forms.Button();
+			this.listView1 = new System.Windows.Forms.ListView();
+			this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.SuspendLayout();
+			// 
+			// btnExit
+			// 
+			this.btnExit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.btnExit.DialogResult = System.Windows.Forms.DialogResult.OK;
+			this.btnExit.Location = new System.Drawing.Point(303, 227);
+			this.btnExit.Name = "btnExit";
+			this.btnExit.Size = new System.Drawing.Size(75, 23);
+			this.btnExit.TabIndex = 0;
+			this.btnExit.Text = "Exit";
+			this.btnExit.UseVisualStyleBackColor = true;
+			this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+			// 
+			// listView1
+			// 
+			this.listView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+			this.listView1.FullRowSelect = true;
+			this.listView1.Location = new System.Drawing.Point(12, 12);
+			this.listView1.Name = "listView1";
+			this.listView1.Size = new System.Drawing.Size(366, 209);
+			this.listView1.TabIndex = 1;
+			this.listView1.UseCompatibleStateImageBehavior = false;
+			this.listView1.View = System.Windows.Forms.View.Details;
+			// 
+			// columnHeader1
+			// 
+			this.columnHeader1.Text = "Time";
+			this.columnHeader1.Width = 123;
+			// 
+			// columnHeader2
+			// 
+			this.columnHeader2.Text = "Event";
+			this.columnHeader2.Width = 237;
+			// 
+			// fMain
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(390, 262);
+			this.Controls.Add(this.listView1);
+			this.Controls.Add(this.btnExit);
+			this.Name = "fMain";
+			this.Text = "I Said Mine!";
+			this.Shown += new System.EventHandler(this.fMain_Shown);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button btnExit;
+		private System.Windows.Forms.ListView listView1;
+		private System.Windows.Forms.ColumnHeader columnHeader1;
+		private System.Windows.Forms.ColumnHeader columnHeader2;
+	}
+}
+
---
+++ ISaidMineCore/ISaidMineGUI/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ISaidMineGUI
+{
+	public partial class fMain : Form
+	{
+		protected ISaidMineCore.Core minerKiller;
+		public fMain()
+		{
+			InitializeComponent();
+			this.minerKiller = new ISaidMineCore.Core(this, @"sgminer", @"C:\Program Files (x86)\sgminer-ltcrabbit-v3\logs\last.log", 10*60);
+			this.minerKiller.KillingMiner += new ISaidMineCore.Core.KillingMinerEventHandler(minerKiller_KillingMiner);
+			this.minerKiller.MinerProcessKilled += new ISaidMineCore.Core.MinerKilledEventHandler(minerKiller_MinerProcessKilled);
+			this.minerKiller.MinerProcessAlive += new ISaidMineCore.Core.MinerAliveEventHandler(minerKiller_MinerProcessAlive);
+		}
+
+		void minerKiller_KillingMiner(object sender, ISaidMineCore.KillingMinerEventArgs e)
+		{
+			log("time to kill..."+e.ProcessName);
+		}
+
+		void minerKiller_MinerProcessAlive(object sender, ISaidMineCore.MinerAliveEventArgs e)
+		{
+			//log("alive");
+		}
+
+		void log(string text)
+		{
+			string[] itemDetails = { DateTime.Now.ToString(), text };
+			ListViewItem item = new ListViewItem(itemDetails);
+			this.listView1.Items.Add(item);
+		}
+
+		void minerKiller_MinerProcessKilled(object sender, ISaidMineCore.MinerKilledEventArgs e)
+		{
+			log("process killed: "+e.ProcessName);
+		}
+
+		private void btnExit_Click(object sender, EventArgs e)
+		{
+			this.Close();
+		}
+
+		private void fMain_Shown(object sender, EventArgs e)
+		{
+			this.minerKiller.StartWatching();
+		}
+	}
+}
